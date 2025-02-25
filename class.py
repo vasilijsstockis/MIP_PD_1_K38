02bc70455003da9dccc726f21852412d1122794f
Vasilijs Stockis <144622104+vasilijsstockis@users.noreply.github.com>
2025-02-26 01:03:16 +0200

    Update class.py

--- class.py
+++ class.py
@@ -1,11 +1,14 @@
 class gameNode:
     def __init__(self, scoreFirst, scoreSecond, scoreBank
-                 , currentValue):
+                 , currentValue,turn):
         
         #Punktu skaita glabāšana
         self.scoreFirst  :int = scoreFirst
         self.scoreSecond :int = scoreSecond
         self.scoreBank   :int = scoreBank
+
+        #kam ir kārta
+        self.turn :bool = turn
 
         #Skaitlis
         self.currentValue :int = currentValue
@@ -15,8 +18,57 @@ class gameNode:
         self.nodeThree  :gameNode = None
         self.nodeFour   :gameNode = None
 
-        #kam ir kārta?
-        #self.Turn :bool
-
         #self.previousNode :gameNode
+    def __str__(self):
+        return f"Number is {self.currentValue}\nFirst player score is: {self.scoreFirst}\nSecond player score is: {self.scoreSecond}\nBank: {self.scoreBank}\nfirts's turn == {self.turn}\n----------------------------------"
+
+
+def populateTree(start : gameNode, maxValue : int) ->None:
+    if(start.currentValue>=maxValue):
+        ## last node is an end node, that means the previous player achived it. Therefore turn is oposite
+        if(start.turn):
+            start.scoreSecond=start.scoreSecond+start.scoreBank
+        else:
+            start.scoreFirst=start.scoreFirst+start.scoreBank
+
+        start.scoreBank=0
+        return
+    else:
+        bank=0
+        score1=0
+        score2=0
+        if (not (start.currentValue%5 or start.currentValue%10)):
+            bank=1
+        if (not start.currentValue%2):
+            if(start.turn):
+                score1=-1
+            else:
+                score2=-1
+        else:
+            if(start.turn):
+                score1=1
+            else:
+                score2=1
+        
+        start.nodeTwo=gameNode(start.scoreFirst+score1, start.scoreSecond+score2, start.scoreBank+bank, start.currentValue*2, not start.turn)
+        start.nodeThree=gameNode(start.scoreFirst+score1, start.scoreSecond+score2, start.scoreBank+bank, start.currentValue*3, not start.turn)
+        start.nodeFour=gameNode(start.scoreFirst+score1, start.scoreSecond+score2, start.scoreBank+bank, start.currentValue*4, not start.turn)
+
+        populateTree(start.nodeTwo,   maxValue)
+        populateTree(start.nodeThree, maxValue)
+        populateTree(start.nodeFour,  maxValue)
     
+
+if __name__ == "__main__":
+    x=gameNode(0,0,0,20,True)
+    populateTree(x,5000)
+
+    while(x is not None):
+        print(x)
+        s=int(input("2, 3 or 4\n"))
+        if(s==2):
+            x=x.nodeTwo
+        if(s==3):
+         x=x.nodeThree
+        if(s==4):
+            x=x.nodeFour
